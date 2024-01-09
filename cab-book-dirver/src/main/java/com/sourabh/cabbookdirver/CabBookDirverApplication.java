52840e016e0ae44aafd3960c2347a4f0e84186fd
Sourabh <sourabhmiglani57@gmail.com>
2024-01-09 11:56:26 +0530

    Kafka Poc

---
+++ cab-book-dirver/src/main/java/com/sourabh/cabbookdirver/CabBookDirverApplication.java
@@ -0,0 +1,13 @@
+package com.sourabh.cabbookdirver;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CabBookDirverApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CabBookDirverApplication.class, args);
+	}
+
+}
---
+++ cab-book-dirver/src/test/java/com/sourabh/cabbookdirver/CabBookDirverApplicationTests.java
@@ -0,0 +1,13 @@
+package com.sourabh.cabbookdirver;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CabBookDirverApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
